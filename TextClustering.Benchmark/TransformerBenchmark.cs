b2202fd28c3051ed3633329200dc8ab5d540227d
NuoC <nuoc@abyssalminds.com>
2024-09-22 17:41:18 +0200

    Added a benchmark for BertTransformer.

---
+++ TextClustering.Benchmark/TransformerBenchmark.cs
@@ -0,0 +1,49 @@
+using System.Text;
+
+using BenchmarkDotNet.Attributes;
+
+using TextClustering.Embedding.Transformer;
+
+namespace TextClustering.Benchmark;
+
+[RPlotExporter]
+public class TransformerBenchmark
+{
+    public List<string> Documents { get; init; } = new();
+
+    private readonly BertTransformer transformer = new();
+
+    [Params(10, 100, 1_000)]
+    public int DatasetSize;
+
+    [GlobalSetup]
+    public void Setup()
+    {
+        var random = new Random();
+        for (int i = 0; i < DatasetSize; ++i)
+        {
+            Documents.Add(GenerateRandomDocument(random, 2, 512));
+        }
+    }
+
+    private static string GenerateRandomDocument(Random random, int minLength, int maxLength)
+    {
+        int length = random.Next(minLength, maxLength + 1);
+        var sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++)
+        {
+            char c = (char)random.Next(32, 127); // ASCII printable characters
+            _ = sb.Append(c);
+        }
+
+        return sb.ToString();
+    }
+
+    [Benchmark]
+    public void Transform()
+    {
+        _ = transformer.Transform(Documents);
+    }
+
+}
