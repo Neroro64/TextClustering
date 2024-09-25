ad66c38aec3110492d7658e651619cd836db0d50
NuoC <nuoc@abyssalminds.com>
2024-09-25 22:25:39 +0200

    Completing the CosineSimilarityBenchmark.

--- TextClustering.Benchmark/CosineSimilarityBenchmark.cs
+++ TextClustering.Benchmark/CosineSimilarityBenchmark.cs
@@ -1,3 +1,5 @@
+using System.Numerics;
+
 using BenchmarkDotNet.Attributes;
 
 using Embedding;
@@ -7,15 +9,17 @@ using NumpyDotNet;
 
 namespace TextClustering.Benchmark;
 
-[RPlotExporter]
 public class CosineSimilarityBenchmark
 {
-    private const int VectorSize = 128;
-    private List<(DenseVector, DenseVector)> DataSet { get; init; } = [];
+    private List<(DenseVector, DenseVector)> DataSet1 { get; init; } = [];
     private List<(ndarray, ndarray)> DataSet2 { get; init; } = [];
     private List<(SparseVector, SparseVector)> DataSet3 { get; init; } = [];
+    private List<(Vector<float>, Vector<float>)> DataSet4 { get; init; } = [];
 
-    [Params(100_000)]
+    [Params(128, 256, 512, 1024)]
+    public int VectorSize;
+
+    [Params(10, 100, 1_000, 10_000, 100_000, 1_000_000)]
     public int DatasetSize;
 
     [GlobalSetup]
@@ -25,41 +29,44 @@ public class CosineSimilarityBenchmark
         var random = new Random();
         for (int i = 0; i < DatasetSize; ++i)
         {
-            DataSet.Add(GenerateRandomFloatVectors(random, -100f, 100f));
-            DataSet2.Add(GenerateRandomFloatNdArray(random, -100f, 100f));
-            DataSet3.Add(GenerateRandomFloatVectors3(random, -100f, 100f));
+            DataSet1.Add(GenerateRandomFloatArrays(random, VectorSize, -100f, 100f));
+            DataSet2.Add(GenerateRandomFloatNdArray(random, VectorSize, -100f, 100f));
+            DataSet3.Add(GenerateRandomFloatSparseVectors(random, VectorSize, -100f, 100f));
+            DataSet4.Add(GenerateRandomFloatVectors(random, VectorSize, -100f, 100f));
         }
     }
 
-    private static (DenseVector, DenseVector) GenerateRandomFloatVectors(Random random, float minValue, float maxValue)
+    private static float GenerateRandomFloat(Random random, float minValue, float maxValue) => ((float)random.NextDouble() * (maxValue - minValue)) + minValue;
+
+    private static (DenseVector, DenseVector) GenerateRandomFloatArrays(Random random, int vectorSize, float minValue, float maxValue)
     {
-        float[] vector1 = new float[VectorSize];
-        float[] vector2 = new float[VectorSize];
-        for (int i = 0; i < VectorSize; ++i)
+        float[] vector1 = new float[vectorSize];
+        float[] vector2 = new float[vectorSize];
+        for (int i = 0; i < vectorSize; ++i)
         {
-            vector1[i] = ((float)random.NextDouble() * (maxValue - minValue)) + minValue;
-            vector2[i] = ((float)random.NextDouble() * (maxValue - minValue)) + minValue;
+            vector1[i] = GenerateRandomFloat(random, minValue, maxValue);
+            vector2[i] = GenerateRandomFloat(random, minValue, maxValue);
         }
         return (new(vector1), new(vector2));
     }
 
-    private static (ndarray, ndarray) GenerateRandomFloatNdArray(Random random, float minValue, float maxValue)
+    private static (ndarray, ndarray) GenerateRandomFloatNdArray(Random random, int vectorSize, float minValue, float maxValue)
     {
-        float[] vector1 = new float[VectorSize];
-        float[] vector2 = new float[VectorSize];
-        for (int i = 0; i < VectorSize; ++i)
+        float[] vector1 = new float[vectorSize];
+        float[] vector2 = new float[vectorSize];
+        for (int i = 0; i < vectorSize; ++i)
         {
-            vector1[i] = ((float)random.NextDouble() * (maxValue - minValue)) + minValue;
-            vector2[i] = ((float)random.NextDouble() * (maxValue - minValue)) + minValue;
+            vector1[i] = GenerateRandomFloat(random, minValue, maxValue);
+            vector2[i] = GenerateRandomFloat(random, minValue, maxValue);
         }
         return (np.array(vector1), np.array(vector2));
     }
 
-    private static (SparseVector, SparseVector) GenerateRandomFloatVectors3(Random random, float minValue, float maxValue)
+    private static (SparseVector, SparseVector) GenerateRandomFloatSparseVectors(Random random, int vectorSize, float minValue, float maxValue)
     {
         Dictionary<int, float> vector1 = [];
         Dictionary<int, float> vector2 = [];
-        for (int i = 0; i < VectorSize; ++i)
+        for (int i = 0; i < vectorSize; ++i)
         {
             vector1.Add(i, ((float)random.NextDouble() * (maxValue - minValue)) + minValue);
             vector2.Add(i, ((float)random.NextDouble() * (maxValue - minValue)) + minValue);
@@ -67,10 +74,22 @@ public class CosineSimilarityBenchmark
         return (new(vector1), new(vector2));
     }
 
+    private static (Vector<float>, Vector<float>) GenerateRandomFloatVectors(Random random, int vectorSize, float minValue, float maxValue)
+    {
+        float[] vector1 = new float[vectorSize];
+        float[] vector2 = new float[vectorSize];
+        for (int i = 0; i < vectorSize; ++i)
+        {
+            vector1[i] = GenerateRandomFloat(random, minValue, maxValue);
+            vector2[i] = GenerateRandomFloat(random, minValue, maxValue);
+        }
+        return (new(vector1), new(vector2));
+    }
+
     [Benchmark]
     public void CompulteCosineSimilarityOnFloatArray()
     {
-        foreach (var (vector1, vector2) in DataSet)
+        foreach (var (vector1, vector2) in DataSet1)
         {
             _ = CosineSimilarity.CalculateDistance(vector1, vector2);
         }
@@ -81,7 +100,7 @@ public class CosineSimilarityBenchmark
     {
         foreach (var (vector1, vector2) in DataSet2)
         {
-            _ = np.dot(vector1, vector2) / np.sqrt(np.dot(vector1, vector1) + np.dot(vector2, vector2));
+            _ = np.dot(vector1, vector2) / (np.sqrt(np.dot(vector1, vector1)) * np.sqrt(np.dot(vector2, vector2)));
         }
     }
 
@@ -91,6 +110,15 @@ public class CosineSimilarityBenchmark
         foreach (var (vector1, vector2) in DataSet3)
         {
             _ = CosineSimilarity.CalculateDistance(vector1, vector2);
+        }
+    }
+
+    [Benchmark]
+    public void ComputeCosineSimilarityOnVector()
+    {
+        foreach (var (vector1, vector2) in DataSet4)
+        {
+            _ = Vector.Dot(vector1, vector2) / (Math.Sqrt(Vector.Dot(vector1, vector1)) * Math.Sqrt(Vector.Dot(vector2, vector2)));
         }
     }
 }
