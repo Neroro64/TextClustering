1a0b32a4092d464b02a09e80ff46ad4cb68311a8
NuoC <nuoc@abyssalminds.com>
2024-09-25 23:48:45 +0200

    Added custom hash function for the embedding vectors.

--- TextClustering/Embedding/EmbeddingVector.cs
+++ TextClustering/Embedding/EmbeddingVector.cs
@@ -4,7 +4,7 @@ namespace Embedding;
 /// Represents an interface for embedding vectors, providing methods to calculate distances between vectors.
 /// </summary>
 /// <typeparam name="TVector">The type of the vector.</typeparam>
-public interface IEmbeddingVector<TVector>
+public interface IEmbeddingVector<TVector> : IEquatable<IEmbeddingVector<TVector>>
 {
     /// <summary>
     /// Calculates the distance between this vector and another vector using a specified distance metric.
@@ -28,10 +28,38 @@ public record DenseVector(float[] Data) : IEmbeddingVector<DenseVector>
     /// <inheritdoc />
     public int Length => Data.Length;
     public float this[int index] => Data[index];
+
+    private int _hashCode = -1;
+
     /// <inheritdoc />
     public double DistanceTo(DenseVector other, Func<DenseVector, DenseVector, float> calculateDistance)
     {
         return calculateDistance(this, other);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            if (_hashCode != -1)
+            {
+                return _hashCode;
+            }
+
+            const int prime = 31;
+            int hash = 17;
+            foreach (float value in Data)
+            {
+                hash = (hash * prime) + BitConverter.ToInt32(BitConverter.GetBytes(value), 0);
+            }
+            _hashCode = hash;
+            return hash;
+        }
+    }
+
+    bool IEquatable<IEmbeddingVector<DenseVector>>.Equals(IEmbeddingVector<DenseVector>? other)
+    {
+        return GetHashCode() == other?.GetHashCode();
     }
 }
 
@@ -45,9 +73,37 @@ public record SparseVector(Dictionary<int, float> Data) : IEmbeddingVector<Spars
     public float this[int index] => Data[index];
     public bool ContainsKey(int key) => Data.ContainsKey(key);
 
+    private int _hashCode = -1;
+
     /// <inheritdoc />
     public double DistanceTo(SparseVector other, Func<SparseVector, SparseVector, float> calculateDistance)
     {
         return calculateDistance(this, other);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            if (_hashCode != -1)
+            {
+                return _hashCode;
+            }
+
+            const int prime = 31;
+            int hash = 17;
+            foreach (var kvp in Data)
+            {
+                hash = (hash * prime) + kvp.Key.GetHashCode();
+                hash = (hash * prime) + kvp.Value.GetHashCode();
+            }
+            _hashCode = hash;
+            return hash;
+        }
+    }
+
+    bool IEquatable<IEmbeddingVector<SparseVector>>.Equals(IEmbeddingVector<SparseVector>? other)
+    {
+        return GetHashCode() == other?.GetHashCode();
     }
 }
